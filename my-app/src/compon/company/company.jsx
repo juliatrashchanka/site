c65b1196caeb5f4879b04114ed1dfa1cad6f4fa1
ST_DOMAIN\Trashchanka_YU <pt195.085al2o3>
2022-06-29 16:55:45 +0300

    company design services

--- my-app/src/compon/company/company.jsx
+++ my-app/src/compon/company/company.jsx
@@ -58,17 +58,25 @@ function Company() {
 
     <div className={style.company_inf}>
       
-      <img className={style.inf_img} src={  "https://www.prereal.com/uploads/load-imagetools.jpeg"}></img>
-     
+      <img className={style.inf_img} src={  "https://i.insider.com/5e137276855cc261a90b8513?width=700"}></img>
+      {/*
+      https://www.prereal.com/uploads/load-imagetools.jpeg */}
       <div className={style.inf_text_box}>
         <div className={style.inf_text__title}><h2>О компании</h2></div>
         <div className={style.inf_text}>
-          <p>ООО "Снектрейд" - надежное строительное предприятие, которое предоставляет услуги в сфере ремонтов, строительства, реконструкции.          </p>
+          <p>ООО "Снектрейд" - надежное строительное предприятие, которое предоставляет услуги в сфере ремонтов, строительства, реконструкции.</p>
           <p>Наши работники - опытные строители, инженеры и прорабы, которые имеют высшее строительное образование, допуск к специализированным работам, отлично выполняют любые строительные работы.</p>
-          <p>В перечень выполняемых работ входит весь комплекс строительных работ. </p>
+          <p>Главная задача компании - обеспечение качества в строительстве и производстве. Поэтому мы тщательно отбираем поставщиков сырья и материалов для наших объектов, и контролируем наших работников.</p>
+          <p>Предоставляем весь комплекс строительных работ. </p>
+          
           </div>
       </div>
 
+    </div>
+
+
+    <div className={style.services_box}>
+      <div className={style.services_box_title}>chj</div>
     </div>
 
 
